aac4fca91d62023f3f5b7f71a20ef462d37d216e
Toni Toth <ttoth009@gmail.com>
2023-11-07 23:38:07 -0500

    made some asthetic changes to the unit card. committing before implementing infinite scroll

--- functions.php
+++ functions.php
@@ -1,14 +1,24 @@
-function condoapptheme_enqueue_styles() {
+<?php
+// Your PHP opening tag
+
+// Theme setup function
+function condoapptheme_setup() {
+    // Add support for various WordPress features here if needed
+}
+
+add_action( 'after_setup_theme', 'condoapptheme_setup' );
+
+// Enqueue styles and scripts
+function condoapptheme_enqueue_styles_scripts() {
     // Enqueue Bootstrap CSS
     wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
     
     // Enqueue your theme stylesheet
     wp_enqueue_style('condoapptheme-style', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'condoapptheme_enqueue_styles');
-
-function condoapptheme_enqueue_scripts() {
+    
     // Enqueue Bootstrap JS and Popper.js
     wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', ['jquery'], null, true);
 }
-add_action('wp_enqueue_scripts', 'condoapptheme_enqueue_scripts');
+
+add_action('wp_enqueue_scripts', 'condoapptheme_enqueue_styles_scripts');
+
--- page-main.php
+++ page-main.php
@@ -22,41 +22,67 @@ get_header(); ?>
                     FROM condo_app.pre_con_unit_database_20230827_v4 u
                         LEFT JOIN condo_app.pre_con_pdf_jpg_database_20230827 j on j.pdf_link = u.floor_plan_link
                     LIMIT 5
-                    ");
+                ");
                 
                 foreach ($units as $unit) {
-                    // Ensure you have a default image in case the floor_plan_link is empty
-                    $default_image = 'path_to_default_image.jpg'; // Replace with actual default image path
+                    // Default image if floor_plan_link is empty
+                    $default_image = get_template_directory_uri() . '/images/default-floorplan.jpg';
                     $image = !empty($unit->jpg_link) ? esc_url($unit->jpg_link) : $default_image;
-                    ?>
+                    // Placeholder data for investment summary
+                    $annualized_return = 'TBD'; // To be defined
+                    $pre_occupancy_deposit = 'TBD';
+                    $cash_on_cash_return = 'TBD';
+                    $projected_rent = 'TBD';
+                    $holding_period = 'TBD';
+                    $projected_appreciation = 'TBD';
+            ?>
 
-                    <div class="card mb-3">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="<?php echo $image; ?>" class="card-img" alt="Floor Plan">
-                            </div>
-                            <div class="col-md-5">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo esc_html($unit->project); ?></h5>
-                                    <p class="card-text"><?php echo esc_html($unit->model); ?></p>
-                                    <p class="card-text"><small class="text-muted"><?php echo esc_html($unit->price); ?></small></p>
-                                    <!-- Add more details here -->
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="card-body">
-                                    <h5 class="card-title">Investment Summary</h5>
-                                    <p class="card-text">Annualized Return: Placeholder</p>
-                                    <!-- More investment details here -->
-                                    <button class="btn btn-primary">Speak to an Agent</button>
-                                </div>
-                            </div>
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <!-- Floor Plan Image Section -->
+                    <div class="col-md-4">
+                        <div class="image-header">
+                            <!-- Placeholder for the image header -->
+                            <span><?php echo esc_html($unit->bedrooms); ?> beds | <?php echo esc_html($unit->bathrooms); ?> baths | <?php echo esc_html($unit->interior_size); ?> sqft interior</span>
+                        </div>
+                        <img src="<?php echo $image; ?>" class="card-img" alt="Floor Plan">
+                    </div>
+
+                    <!-- Unit Details Section -->
+                    <div class="col-md-5">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo esc_html($unit->project); ?> | Model <?php echo esc_html($unit->model); ?> | Unit #<?php echo esc_html($unit->unit_number); ?></h5>
+                            <p class="card-text">Starting from $<?php echo esc_html(number_format($unit->price)); ?></p>
+                            <p class="card-text">Occupancy: <?php echo esc_html($unit->occupancy_date); ?></p>
+                            <p class="card-text">Developer: <?php echo esc_html($unit->developer); ?></p>
+                            <p class="card-text"><?php echo esc_html($unit->address); ?></p>
+                            <!-- Add more unit-specific details here -->
                         </div>
                     </div>
-                    <?php
+
+                    <!-- Investment Summary Section -->
+                    <div class="col-md-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Investment Summary</h5>
+                            <p class="card-text">+<?php echo $annualized_return; ?>% annualized return</p>
+                            <p class="card-text"><?php echo $pre_occupancy_deposit; ?>% pre-occupancy deposit</p>
+                            <p class="card-text">$<?php echo $cash_on_cash_return; ?> Cash-on-cash return</p>
+                            <p class="card-text">$<?php echo $projected_rent; ?> projected rent</p>
+                            <p class="card-text"><?php echo $holding_period; ?> year holding period</p>
+                            <p class="card-text"><?php echo $projected_appreciation; ?>% Projected appreciation</p>
+                            <!-- More investment details here -->
+                            <button class="btn btn-primary">Speak to an Agent</button>
+                            <!-- Icons for interaction -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php
                 }
             ?>
         </section>
+
 
 
     </main><!-- #main -->
