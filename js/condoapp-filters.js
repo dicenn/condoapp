6b2ea6ea59752b236641380cd8cdbcc881e5b38c
Toni Toth <ttoth009@gmail.com>
2023-11-09 00:37:24 -0500

    deduped a redundant filter file

--- js/condoapp-filters.js
+++ js/condoapp-filters.js
@@ -10,8 +10,31 @@ jQuery(document).ready(function($) {
         prettify_enabled: true,
         prettify_separator: ",",
         onFinish: function(data) {
-            // Here you can add the AJAX call to filter the units based on the selected price range
-            // For now, you can just log the selected values to confirm it's working
+            $("#price-range").on("change", function () {
+                var $this = $(this),
+                    value = $this.prop("value").split(";");
+            
+                $.ajax({
+                    url: condoapp_ajax.ajax_url, // This should be already localized in your main AJAX script
+                    type: 'POST',
+                    data: {
+                        action: 'filter_units_by_price', // This is the WordPress hook you'll use
+                        nonce: condoapp_ajax.nonce, // Nonce for security
+                        price_range: {
+                            min: value[0],
+                            max: value[1]
+                        }
+                    },
+                    success: function(response) {
+                        // This is where you'll handle the response
+                        // For now, let's just log it to the console
+                        console.log(response);
+                    },
+                    error: function(error) {
+                        console.error('Error:', error);
+                    }
+                });
+            });
             console.log("Selected range: " + data.from + " to " + data.to);
         }
     });
